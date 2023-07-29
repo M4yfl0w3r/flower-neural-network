ddeab87b5ac0c5fd87271047bbbd5ec4544c56b0
M4yfl0w3r <mr.mayflower15@gmail.com>
2023-07-29 22:40:29 +0200

    * Generate random weights & biases

--- net.R
+++ net.R
@@ -8,4 +8,24 @@ y <- iris[  , 5]
 
 iris$Species <- as.numeric(iris$Species)
 
-print(y)
+num_inputs <- 4
+num_neurons <- ncol(x)
+
+generate_random_weights <- function(num_inputs, num_neurons) {
+    weights <- vector('list', num_inputs)
+
+    for (i in 1:num_inputs) {
+        weights[[i]] <- rnorm(num_neurons)
+    }
+
+    return(weights)
+}
+
+generate_random_biases <- function(num_neurons) {
+    biases <- rnorm(num_neurons)
+    return(biases)
+}
+
+weights <- generate_random_weights(num_inputs, num_neurons)
+biases <- generate_random_biases(num_neurons)
+

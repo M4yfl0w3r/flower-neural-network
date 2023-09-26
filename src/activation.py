a5eb38b036e87afaa36cb715c276f4d7edc1546d
M4yfl0w3r <mr.mayflower15@gmail.com>
2023-09-26 22:48:42 +0200

    * add backward function

--- src/activation.py
+++ src/activation.py
@@ -2,18 +2,38 @@ import numpy as np
 
 
 class ReLU:
+    
+    def __init__(self):
+        self.input: np.ndarray = None
 
     def forward(self, input: np.ndarray) -> np.ndarray:
         output: np.ndarray = np.maximum(0, input)
+        self.input = input
         return output
-    
+
+    def backward(self, gradient: np.ndarray) -> np.ndarray:
+        output: np.ndarray = np.where(self.input <= 0, 0, 1) * gradient
+        return output
+
 
 class Softmax:
 
+    def __init__(self):
+        self.output = None
+
     def forward(self, input: np.ndarray) -> np.ndarray:
         exp_values: np.ndarray = np.exp(input.data)
-        output: np.ndarray = exp_values / np.sum(exp_values, axis = 0)
-        return output
+        self.output = exp_values / np.sum(exp_values, axis = 0)
+        return self.output
     
-    def backward(self):
-        pass
+    def backward(self, gradient: np.ndarray) -> np.ndarray:
+        softmax_gradient: list = []
+        
+        def grad_(input: np.ndarray) -> np.ndarray:
+            s: np.ndarray = input.reshape(-1, 1)
+            return np.diagflat(s) - np.dot(s, s.T)
+        
+        for output, incoming_grad in zip(self.output, gradient):
+            softmax_gradient.append(np.dot(grad_(output), incoming_grad))
+
+        return np.array(softmax_gradient)
--- src/network.py
+++ src/network.py
@@ -6,7 +6,21 @@ class DenseLayer:
     def __init__(self, num_inputs: int, num_neurons: int):
         self.weights: np.ndarray = np.random.rand(num_inputs, num_neurons)
         self.biases: np.ndarray = np.random.rand(1, num_neurons)
+        
+        self.input: np.ndarray = None
+        self.output: np.ndarray = None
+
+        self.weights_gradient: np.ndarray = None
+        self.biases_gradient: np.ndarray = None
+        self.input_gradient: np.ndarray = None
 
     def forward(self, input: np.ndarray) -> np.ndarray:
-        output: np.ndarray = np.dot(input, self.weights) + self.biases
-        return output
+        self.output = np.dot(input, self.weights) + self.biases
+        self.input = input
+        return self.output
+    
+    def backward(self, gradient: np.ndarray) -> np.ndarray:
+        self.weights_gradient = np.dot(np.transpose(self.input), gradient)
+        self.biases_gradient = np.sum(1 * gradient, axis = 0)
+        self.input_gradient = np.dot(gradient, np.transpose(self.weights))
+        return self.input_gradient

32d6c8cdb6de86fd0b3cef846db9ae98480cd685
M4yfl0w3r <mr.mayflower15@gmail.com>
2024-01-02 14:49:43 +0100

    * download iris dataset using lua script

--- build.lua
+++ build.lua
@@ -1,7 +1,29 @@
 #!/bin/lua
 
+os.execute("mkdir -p assets/iris")
+
+function file_exists(name)
+    local f = io.open(name, 'r')
+    if f ~= nil then 
+        io.close(f)
+        return true 
+    else 
+        return false 
+    end
+end
+
+local dataset_path = 'iris_dataset'
+
+if not file_exists('assets/iris/iris.data') then
+    local dataset_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    os.execute('wget ' .. dataset_url .. ' -O ' .. 'iris.data')
+
+    os.execute('mv iris.data assets/iris/')
+end
+
 os.execute("mkdir -p build")
 os.execute("premake5 --file=premake5.lua gmake")
 os.execute("make")
 os.execute("./build/Debug/Net")
 os.execute("rm Makefile")
+

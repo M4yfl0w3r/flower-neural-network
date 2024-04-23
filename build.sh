ff68c55752c84580cbb8a64313df9eab85f2720c
M4yfl0w3r <mr.mayflower15@gmail.com>
2024-04-23 20:57:03 +0200

    * Change premake to cmake since there is no support for C++23

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p assets/iris
+
+function file_exists() {
+    if [ -e "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if ! file_exists "assets/iris/iris.data"; then
+    dataset_url="https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    wget "$dataset_url" -O "iris.data"
+    mv "iris.data" assets/iris/
+fi
+
+mkdir -p build
+cd build
+cmake ..
+make
+./flower_neural_network
+

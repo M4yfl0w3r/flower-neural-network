27b111bd34399f29e4685753dca03722df3ab112
M4yfl0w3r <mr.mayflower15@gmail.com>
2024-10-28 21:00:19 +0100

    *

--- main.cxx
+++ main.cxx
@@ -8,41 +8,39 @@ import dense_layer;
 auto main() -> int
 {
     using namespace Loss;
+    namespace rv = std::ranges::views;
 
-    auto dataset = Dataset(Config::irisPath);
-    const auto& [data, labels] = dataset.GetRandomBatch();
-    const auto rows = Tensor<float, { Config::batchSize, Config::dataCols }>(data);
-    const auto cols = ColumnTensor<Config::batchSize>(labels);
+    const auto dataset{ Dataset(Config::irisPath) };
+    const auto& [data, labels] { dataset.GetRandomBatch() };
+    const auto rows{ Tensor<float, { Config::batchSize, Config::dataCols }>(data) };
+    const auto cols{ ColumnTensor<Config::batchSize>(labels) };
 
-    static constexpr auto stLayerParams = LayerParams{ .Inputs = Config::dataCols, .Neurons = 10 };
-    static constexpr auto ndLayerParams = LayerParams{ .Inputs = 10, .Neurons = Config::numClasses };
+    auto st { DenseLayer< LayerParams{ .Inputs =   4, .Neurons = 10 },
+                          LayerParams{ .Inputs =  10, .Neurons =  4 },
+                          LayerParams{ .Inputs =  10, .Neurons = 10 }
+                        > {}
+    };
 
-    auto st   = DenseLayer<
-                            stLayerParams,
-                            LayerParams{ .Inputs = 10, .Neurons = 4 }, // prev layer params
-                            LayerParams{ .Inputs = 10, .Neurons = 10 }  // next layer params
-                          > {};
+    auto nd { DenseLayer< LayerParams{ .Inputs = 10, .Neurons =  3 },  // 3 - num classes
+                          LayerParams{ .Inputs = 10, .Neurons = 10 },  // prev layer params
+                          LayerParams{ .Inputs = 10, .Neurons =  3 }   // next layer params
+                        > {}
+    };
 
-    auto nd   = DenseLayer<
-                            ndLayerParams,
-                            LayerParams{ .Inputs = 10, .Neurons = 10 }, // prev layer params
-                            LayerParams{ .Inputs = 10, .Neurons = 3 }   // next layer params
-                          > {};
+    auto loss{ CategoricalCrossEntropy() };
 
-    auto loss = Loss::CategoricalCrossEntropy();
+    for (auto i : rv::iota(0, Config::epochs)) {
+        auto o1{ st.ForwardReLU(rows) };
+        auto o2{ nd.ForwardSoftmax(o1) };
 
-    for (auto i : std::ranges::iota_view(0, Config::epochs)) {
-        auto o1 = st.ForwardReLU(rows);
-        auto o2 = nd.ForwardSoftmax(o1);
-
-        const auto lossValue = loss.Forward< LayerParams{ .Inputs = 10, .Neurons = 3 } >(o2, cols);
-        const auto accValue  = Accuracy(&o2, cols) * 100;
+        const auto lossValue{ loss.Forward< LayerParams{ .Inputs = 10, .Neurons = 3 } >(o2, cols) };
+        const auto accValue{ Accuracy(&o2, cols) * 100 };
 
         std::println("Loss = {0} | Accuracy = {1}%", lossValue.At(0, 0), accValue);
 
-        auto o3 = loss.Backward< LayerParams{ .Inputs = 10, .Neurons = 3 } >(o2);
+        auto o3{ loss.Backward< LayerParams{ .Inputs = 10, .Neurons = 3 } >(o2) };
 
-        auto o4 = nd.BackwardSoftmax(o3);
-        auto o5 = st.BackwardReLU(o4);
+        auto o4{ nd.BackwardSoftmax(o3) };
+        auto o5{ st.BackwardReLU(o4) };
     }
 }
